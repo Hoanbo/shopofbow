86233fea851bebd8eb0c82de820ba0f8e429d9d6
Hoanbo <hoankb4@gmail.com>
2026-07-31 02:55:59 +0700

    feat: initialize root entry point with providers and implement main site layout component

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,22 +3,20 @@ import Header from './Header';
 import Footer from './Footer';
 import MobileNav from './MobileNav';
 import { ContactFab } from './ContactButtons';
-import { ToastProvider } from './Toast';
 
 export default function Layout() {
   return (
-    <ToastProvider>
-      <div className="flex min-h-dvh flex-col">
-        <Header />
-        <main className="flex-1 pb-20 md:pb-0">
-          <Outlet />
-        </main>
-        <Footer />
-        <MobileNav />
-        <ContactFab />
-        <ScrollRestoration />
-      </div>
-    </ToastProvider>
+    <div className="flex min-h-dvh flex-col">
+      <Header />
+      <main className="flex-1 pb-20 md:pb-0">
+        <Outlet />
+      </main>
+      <Footer />
+      <MobileNav />
+      <ContactFab />
+      <ScrollRestoration />
+    </div>
   );
 }
+
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { ContactProvider } from './context/ContactContext'
+import { ToastProvider } from './components/Toast'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ContactProvider>
-      <App />
-    </ContactProvider>
+    <ToastProvider>
+      <ContactProvider>
+        <App />
+      </ContactProvider>
+    </ToastProvider>
   </React.StrictMode>,
 )
